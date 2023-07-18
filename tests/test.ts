c15017e2de748fd393c52bd28001b5b69816265e
Shinichi Okada <okada.shin@gmail.com>
2023-07-18 08:10:23 +0200

    fix: update flowbite-svelte v0.40.1, add @floating-ui/dom remove flowbite and popper, add tests

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,10 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
   await page.goto('/');
-  await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Svelte Boxicons' })).toBeVisible();
+});
+
+test('Icons page has expected h1', async ({ page }) => {
+  await page.goto('/icons');
+  await expect(page.getByRole('heading', { name: 'Svelte Boxicons: Icons' })).toBeVisible();
 });
