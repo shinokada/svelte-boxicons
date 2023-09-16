2792c01725d00f72d9214050d0ffe693296f44d6
Shinichi Okada <okada.shin@gmail.com>
2023-09-16 06:53:34 +0200

    feat: using README in +page.svelte

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [sveltekit()],
+  server: {
+    fs: { allow: [`..`] }, // needed to import from $root
+  },
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   }
